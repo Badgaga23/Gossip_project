5e15a9adc757c53935139a9357f1a1f2a440543a
Badgaga23 <altgaetan@hotmail.com>
2020-10-27 02:48:40 +0100

    last commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,22 @@
 class StaticPagesController < ApplicationController
   def team
-    @first_name
+    
   end
 
   def contact
+
+  end
+
+  def gossip
+    @gossip = Gossip.all
+  end
+
+  def user
+    @user = User.all
+  end
+
+  def home
+    @gossip = Gossip.all
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'gossip/gossip'
+  get 'user/user'
+  get '/' ,to: "static_pages#home"
   get 'team',to: "static_pages#team"
   get 'contact',to: "static_pages#contact"
+  get 'gossip', to: "static_pages#gossip"
+  get 'user', to: "static_pages#user"
   get 'welcome/:first_name', to: 'welcome#first_name'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
